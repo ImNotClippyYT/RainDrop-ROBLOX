2f5b9dd38fbd0fff33e95901f46292eee6c1e05a
ImNotClippy <appus321321@outlook.com>
2026-01-04 22:35:34 +0000

    AutoSetup
    Auto

---
+++ AutoSetup.lua
@@ -0,0 +1,12 @@
+-- ENABLE HTTPSERVICE (game Settings -> Security -> HttpService)
+
+local Http = game:GetService("HttpService")
+
+local URL = "https://raw.githubusercontent.com/ImNotClippyYT/RainDrop-Auto-Creator-1/refs/heads/main/xd.lua"
+
+local codeinit = Http:GetAsync(URL)
+
+
+local module = Instance.new("ModuleScript")
+module.Source = source
+module.Parent = game.ReplicatedStorage
